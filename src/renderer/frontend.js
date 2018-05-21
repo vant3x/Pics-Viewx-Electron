df20594930835ab0a690d99fa9921dcb6675290c
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-05-21 15:36:10 -0500

    Buscador de imagenes

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,5 +1,9 @@
+import url from 'url'
+import path from 'path'
+
 window.addEventListener('load', () => {
   addImagesEvents()
+  searchImagesEvent()
 })
 
 function addImagesEvents () {
@@ -17,4 +21,33 @@ function changeImage (node) {
   document.querySelector('li.selected').classList.remove('selected')
   node.classList.add('selected')
   document.getElementById('image-displayed').src = node.querySelector('img').src
+}
+
+// buscar imagenes
+
+function searchImagesEvent () {
+  const searchBox = document.getElementById('search-box')
+
+  searchBox.addEventListener('keyup', function () {
+    const regex = new RegExp(this.value.toLowerCase(), 'gi')
+
+    if (this.value.length > 0) {
+      const thumbs = document.querySelectorAll('li.list-group-item img')
+      for (let i = 0, length1 = thumbs.length; i < length1; i++) {
+        const fileUrl = url.parse(thumbs[i].src)
+        const fileName = path.basename(fileUrl.pathname)
+        if (fileName.match(regex)) {
+          thumbs[i].parentNode.classList.remove('hidden')
+        } else {
+          thumbs[i].parentNode.classList.add('hidden')
+        }
+      }
+      selectFirstImage()
+    }
+  })
+}
+
+function selectFirstImage () {
+  const image = document.querySelector('li.list-group-item:not(.hidden)')
+  changeImage(image)
 }
