bdc5db520709d0332ce7b239049563e5585446b9
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-05-29 17:20:28 -0500

    Archivos de directorios

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 'use strict'
-
 // El objeto app nos permite controlar el ciclo de vida de la app
-
 // El objeto BrowserWindows nos permite cargar todo el contenido visual de la app
-
 import { app, BrowserWindow, ipcMain, dialog } from 'electron'
 import devtools from './devtools'
+import fs from 'fs'
+import filesize from 'filesize'
+import isImage from 'is-image'
+import path from 'path'
 
 let win
 
 if (process.env.NODE_ENV === 'development') {
   devtools()
 }
-
 // Imprimiendo un mensaje en la consola antes de salir
 app.on('before-quit', () => {
   console.log('Saliendo...')
@@ -44,7 +44,7 @@ app.on('ready', () => {
   win.loadURL(`file://${__dirname}/renderer/index.html`)
   win.toggleDevTools()
 })
-
+// --- Abrir directorio e imagenes ---
 ipcMain.on('open-directory', (event) => {
   dialog.showOpenDialog(win, {
     title: 'Seleccione la nueva ubicación',
@@ -52,6 +52,19 @@ ipcMain.on('open-directory', (event) => {
     properties: ['openDirectory']
   },
   (dir) => {
-    console.log(dir)
+    const images = []
+    if (dir) {
+      fs.readdir(dir[0], (err, files) => {  // eslint-disable-line
+        for (var i = 0, length1 = files.length; i < length1; i++) { // eslint-disable-line
+          if (isImage(files[i])) { // eslint-disable-line
+            let imageFile = path.join(dir[0], files[i])
+            let stats = fs.statSync(imageFile)
+            let size = filesize(stats.size, {round: 0})
+            images.push({filename: files[i], src: `file://${imageFile}`, size: size}) // eslint-disable-line
+          }
+        }
+        console.log(images)
+      })
+    }
   })
-})
+}) // eslint-disable-line
